2d92700ffd33ae5ec90451fc2274e2d2c4dea460
Mike Murray <hellomikemurray@gmail.com>
2015-08-24 14:17:40 -0700

    Convert coffeescript to js

---
+++ client/templates/stripe.js
@@ -0,0 +1,20 @@
+Template.stripe.helpers({
+  packageData: function() {
+    return ReactionCore.Collections.Packages.findOne({
+      name: "reaction-stripe"
+    });
+  }
+});
+
+AutoForm.hooks({
+  "stripe-update-form": {
+    onSuccess: function(operation, result, template) {
+      Alerts.removeSeen();
+      return Alerts.add("Stripe settings saved.", "success");
+    },
+    onError: function(operation, error, template) {
+      Alerts.removeSeen();
+      return Alerts.add("Stripe settings update failed. " + error, "danger");
+    }
+  }
+});
